f43444bdd2aad7f036f19d6c6cd76de6bd8bdd96
TERRY TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGTE@OOCL.COM>
2019-07-12 07:36:18 +0800

    finish project1 project2 and project3

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,27 @@
 package practice02;
 
 public class Student {
+    private String name;
+    private int age;
+private int klass;
+    public Student(String name, int age,int klass) {
+        this.name = name;
+        this.age = age;
+        this.klass=klass;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getKlass(){
+        return klass;
+    }
+    public String introduce() {
+        return "I am a Student. I am at Class " + getKlass() + ".";
+    }
 }
+
--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,25 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+    private String name;
+    private int age;
+
+    public Worker(String name, int age) {
+        super(name,age);
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String introduce() {
+        return "I am a Worker. I have a job.";
+    }
 }
